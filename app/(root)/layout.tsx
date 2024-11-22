5c591ab81fd7f2a36d1111d3794d329edbb6a6ea
samuel mwangi <gitonga755@gmail.com>
2024-11-22 14:42:00 +0300

    implement the sidebars, implement the homepage implement the group

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+
+import Sidebar from "@/components/Sidebar";
+import Image from 'next/image';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const loggedIn = { firstName: 'Adrian ', lastName:
+    'JSM' } ;
+  return (
+    <main className = "flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} /> 
+
+      <div className ="flex size-full flex=col">
+        <div className="root-layout">
+          <image src="/icons/logo.svg" width={30} height={30} alt="menu icon"/>
+          </div>
+      </div>
+    </main>
+  );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,31 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+
+const home = () => {
+  const loggedIn = { firstName: 'Adrian'};
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and 
+            transactions efficiently"
+            
+          />
+           
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks= {1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  )
+}
+
+export default home
